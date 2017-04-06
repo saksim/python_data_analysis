13acbdbe51cdf42570564a37a36f2fbe5cb3a65a
saksim <heweiwei0107@163.com>
2017-04-06 18:03:50 +0800

    learning_curve defination function
    learning_curve defination function

--- sklearn_algorithm_compare.py
+++ sklearn_algorithm_compare.py
@@ -148,5 +148,56 @@ plt.title('ROC curve')
 plt.legend(loc='best')
 plt.show()
 '''
+'''
+from sklearn.model_selection import learning_curve
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(X_train,Y_train)
 
+Y_train_pro = clf.predict_proba(X_train)
+Y_train_label = clf.predict(X_train)
+Y_test_pro = clf.predict_proba(X_test)
+Y_test_label = clf.predict(X_test)
 
+lc = learning_curve(clf,X_train,Y_train_label)
+
+plt.figure()
+title = "Learning Curves (Naive Bayes)"
+plt.title(title)
+plt.xlabel("Training examples")
+plt.ylabel("Score")
+plt.grid()
+'''
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    plt.xlabel(u"训练样本数量")
+    plt.ylabel(u"标签返回值")
+    train_sizes, train_scores, test_scores = learning_curve(
+    estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+	train_scores_mean + train_scores_std, alpha=0.1,
+	color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+	test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+	label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+	label="Cross-validation score")
+    plt.legend(loc="best")
+    return plt
+
+XX,yy= X_train,Y_train
+title = u"基于泰坦尼克号存活名单的学习曲线 (Naive Bayes)"
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+from sklearn.naive_bayes import GaussianNB
+estimator = GaussianNB()
+plot_learning_curve(estimator,title,XX,yy,cv,n_jobs=1)
+plt.show()
